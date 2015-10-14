419e646d8a6aaa78ff03968ddb87af7eb3a0ea01
lohas.le <days365le@gmail.com>
2015-10-14 09:38:54 +0800

    project init

---
+++ 2015/config.rb
@@ -0,0 +1,30 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+output_style = :compact
+relative_assets = true
+line_comments = false
+enable_sourcemaps = true
+sass_options = {:sourcemap => true}
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
